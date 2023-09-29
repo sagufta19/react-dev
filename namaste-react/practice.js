0605e2468d4372cf880e35ca57d2d7f780f52302
Md Imran <imranhacks59@gmail.com>
2023-09-29 10:06:24 +0530

    changes

--- namaste-react/practice.js
+++ namaste-react/practice.js
@@ -3,5 +3,19 @@ import ReactDOM from "react-dom/client";
 
 const heading = <h1>Hello World in react!</h1>;
 console.log(heading);
+
+const title=(
+    <h1 className="head" tabIndex="5">
+        Namste React Using JSX!
+    </h1>
+);
+
+const HeaderComponent=()=>(
+    <div id="container">
+        {title}
+        <h1 className="heading">Namste React</h1>
+    </div>
+);
+
 const root=ReactDOM.createRoot(document.createElementById("root"));
 root.render(heading);
